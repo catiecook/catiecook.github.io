929d213815daf83ec508f32c352c1c32ef4fa60d
catiecook <catiecook888@gmail.com>
2019-05-20 20:15:50 -0600

    total refresh

---
+++ src/components/arrow-animation/ArrowAnimation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './arrow.scss';
+
+function Arrow() {
+  return (
+    <div className="arrow-container">
+      <div className="scroll-arrow"></div>
+      <div className="scroll-arrow"></div>
+      <div className="scroll-arrow"></div>
+    </div>
+  )
+}
+
+export default Arrow;
